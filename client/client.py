e8544cc2b8681c7d9c7f5e5577d41f3bfddb24d5
parsariyahi <pany.parsariyahi@gmail.com>
2023-04-20 23:59:19 +0330

    feature(client): Add client class and main file to run a client

---
+++ client/client.py
@@ -0,0 +1,25 @@
+import sys
+import asyncio
+from conn import Conn
+
+
+if "win" in sys.platform:
+    # RuntimeWarning: Proactor event loop does not implement add_reader family of methods required for zmq.
+    # Registering an additional selector thread for add_reader support via tornado.
+    # Use `asyncio.set_event_loop_policy(WindowsSelectorEventLoopPolicy())` to avoid this warning.
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+class Client:
+
+    def __init__(self, command) -> None:
+        self.command = command
+        self._conn = Conn("localhost", 5555)
+        self.loop = asyncio.get_event_loop()
+
+    def run_command(self):
+        return self.loop.run_until_complete(self.__async_run_command())
+
+    async def __async_run_command(self):
+        async with self._conn as conn :
+            await conn.send(self.command)
+            return await conn.recieve()
---
+++ client/main.py
@@ -0,0 +1,17 @@
+import os, json
+from client import Client
+
+if __name__ == "__main__" :
+    # Get json file name and generate the path
+    json_file_name =  input("Enter your json file name: ")
+    current_path = os.path.dirname(__file__)
+    json_file_path = os.path.join(current_path, json_file_name)
+
+    with open(json_file_path) as file:
+        command = json.loads(
+            file.read()
+        )
+
+    cl = Client(command)
+    resp = cl.run_command()
+    print(resp)
