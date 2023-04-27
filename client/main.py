73750675b7ed10f88f0d5a75e30920e07b8200bc
parsariyahi <pany.parsariyahi@gmail.com>
2023-04-27 12:53:55 +0330

    refactor(client): Moved ``set_event_loop_policy`` to main.py

--- client/main.py
+++ client/main.py
@@ -2,6 +2,13 @@ import os, json
 from client import Client
 
 def main() :
+    if "win" in sys.platform:
+        # If the client is running windows we should add this.
+        # RuntimeWarning: Proactor event loop does not implement add_reader family of methods required for zmq.
+        # Registering an additional selector thread for add_reader support via tornado.
+        # Use `asyncio.set_event_loop_policy(WindowsSelectorEventLoopPolicy())` to avoid this warning.
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
     # Get json file name and generate the path
     json_file_name =  input("Enter your json file name: ")
     current_path = os.path.dirname(__file__)
