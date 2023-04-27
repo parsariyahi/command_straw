0c349aa2db5e3f5e04e8852436ba9377020f540e
parsariyahi <pany.parsariyahi@gmail.com>
2023-04-27 14:27:02 +0330

    core(server): Add ``cleaned_data_for_response`` in Math executer

--- server/core/math.py
+++ server/core/math.py
@@ -1,10 +1,14 @@
 class MathExeciter:
 
     @classmethod
-    def execute(cls, command):
-        return eval(command["expression"])
+    def execute(cls, command: dict) -> dict:
+        expression = command["expression"]
+        result = eval(expression)
 
-    def _clean_data_for_response(cls, result, expression) :
+        return cls._clean_data_for_response(result, expression)
+
+    @classmethod
+    def _clean_data_for_response(cls, result: float, expression: str) -> dict:
         return {
             "given_math_expression": expression,
             "result": result
